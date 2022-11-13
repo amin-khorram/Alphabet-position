10376a9a7387b7eebcf5f1468160945b7417f439
Amin Khorram <akw126@uregina.ca>
2022-11-12 18:26:16 -0600

    Update tesy.py

--- tesy.py
+++ tesy.py
@@ -1,13 +1,13 @@
 import string
-def alphabet_position(inp):
+def alphabet_position(text):
     outot=[]
-    for i in inp:
+    for i in text:
         if not i.isalpha():
             pass
         elif i.isalpha():
             i= i.lower()
             index = list(string.ascii_lowercase).index(i)
             outot.append(index+1)
-    print(f"'{''.join(str(x) for x in outot)}'")
+    return ' '.join(str(e) for e in outot)
 
 alphabet_position("The sunset sets at twelve o' clock.")
